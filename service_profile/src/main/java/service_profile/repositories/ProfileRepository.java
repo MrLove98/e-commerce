f44ba3f01a7f9cda40d3458fbbe234a1b379e529
MrLove98 <mr.nquangtinh@gmail.com>
2024-11-25 10:24:01 +0700

    CRUD service_profile

---
+++ service_profile/src/main/java/service_profile/repositories/ProfileRepository.java
@@ -0,0 +1,19 @@
+package service_profile.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import service_profile.entities.ProfileEntity;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Integer> {
+
+    Optional<ProfileEntity> getByEmail(String email);
+
+    Optional<ProfileEntity> findByEmail(String email);
+
+    Page<ProfileEntity> findAll(Pageable pageable);
+}
